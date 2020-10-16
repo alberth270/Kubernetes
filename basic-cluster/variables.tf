67929155a12960253992c3d46cc18ed599d6a724
alberth270 <36995741+alberth270@users.noreply.github.com>
2020-10-16 15:01:43 -0500

    Update variables.tf

--- basic-cluster/variables.tf
+++ basic-cluster/variables.tf
@@ -1,7 +1,9 @@
 variable "prefix" {
   description = "A prefix used for all resources in this example"
+  default = "SP"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be provisioned"
+  default = "Central US"
 }
